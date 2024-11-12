59f4d5f08b86ab7f4334c0d49a7d22c3738fcf7a
Franco <franbosco12@gmail.com>
2024-11-12 19:02:32 -0300

    loader

--- src/components/Preloader/index.js
+++ src/components/Preloader/index.js
@@ -20,16 +20,8 @@ export default function Preloader() {
     >
       <div className="loader">
         <div className="loader-outter"></div>
-        <div className="loader-inner"></div>
-        <div className="indicator">
-          <svg style={{ width: "16px", height: "12px" }}>
-            <polyline id="back" points="1 6 4 6 6 11 10 1 12 6 15 6"></polyline>
-            <polyline
-              id="front"
-              points="1 6 4 6 6 11 10 1 12 6 15 6"
-            ></polyline>
-          </svg>
-        </div>
+       
+  
       </div>
     </div>
   );
